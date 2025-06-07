9a729e3765d60828a6bc1c166f9806aeba2c7ae2
Duong Hoc IT <duongtungvu2004@gmail.com>
2025-06-07 16:41:20 +0700

    feat: cập nhật base code backend

---
+++ backend/src/main/java/com/rsasignature/RsaSignatureApplication.java
@@ -0,0 +1,11 @@
+package com.rsasignature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsaSignatureApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RsaSignatureApplication.class, args);
+    }
+}
