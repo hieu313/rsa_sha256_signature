baa218ae21de11c5020ae1dc398157f810de0a9d
hieunm3103 <hieunm3103@gmail.com>
2025-06-12 00:02:38 +0700

    backend-feat: add login and register

---
+++ backend/src/main/java/com/RsaSha256SignatureApplication.java
@@ -0,0 +1,11 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsaSha256SignatureApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RsaSha256SignatureApplication.class, args);
+    }
+}
---
+++ backend/src/main/java/com/controller/AuthController.java
@@ -0,0 +1,26 @@
+
+package com.controller;
+
+import com.dto.LoginRequest;
+import com.dto.RegisterRequest;
+import com.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequest request) {
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
+}
